1b53a1983cb9c84963e57f18ec7f69927d8fd117
Mauricio Junior <mjunior1205@gmail.com>
2024-01-24 14:46:41 -0300

    First commit

---
+++ src/test/java/workshopmongo/demo/StockManagerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package workshopmongo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
